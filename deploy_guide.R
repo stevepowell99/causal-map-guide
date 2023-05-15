f83ba01f4f8999e3700c4244f2543eda85053ea9
Steve Powell <steve@pogol.net>
2023-05-15 10:49:05 +0100

    WIP
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    wip
    
    
    WIP
    
    wip
    
    
    wip
    
    
    WIP
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    WIP
    
    WIP
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    WIP
    
    wip
    
    
    docs
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    fuse search
    
    
    no fuse
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    fuse search
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    faq
    
    
    wip
    
    
    WIP
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    changed flag to hashtag
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wuo
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    WIP
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip
    
    
    wip

--- deploy_guide.R
+++ deploy_guide.R
@@ -1,1 +1,6 @@
 rmarkdown::render_site("c:/Users/steve/Dropbox/Projects/causal-map-guide",output_format = 'bookdown::bs4_book', encoding = 'UTF-8')
+
+system("cd c:\\Users\\steve\\Dropbox\\Projects\\causal-map-guide\\")
+system('git add .')
+system('git commit -m "WIP"')
+system('git push')
